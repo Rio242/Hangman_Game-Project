7e11dc1de4523394638ba747f4435ee4d2124efc
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2022-09-27 04:44:51 +0000

    Added while loop to allow the user to continue to guess. Added a live counter

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ word_list = ["aardvarka", "baboon", "camel", "lover", "mango"]
 import random
 
 #function to select random word from list above 
-chosen_word= random.choice(word_list)
+chosen_word = random.choice(word_list)
 print(chosen_word)
 #Below is what I would you in a loop and unspecified number of entries in list 
 #num_words =  len(word_list)-1 
@@ -27,25 +27,40 @@ print(blank_word)
 #convert word string to its own list
 chosenword_list = list(chosen_word)
 print(chosenword_list)
+
+#while loop 
+lives = 6
+while blank_word != chosenword_list: 
+    
 #input to ask user to guess
-user = input("Please guess a letter").lower()
+    user = input("Please guess a letter").lower()
 
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 #Mario- user gets 6 attempts 
 
+
 # for loop to check if guessed letter matches in word. Then if statement is included to replace the blank with letter if correct  
 
 
-
-for n in chosen_word:
+    wrong = 0
+    for n in chosen_word:
     
-    if n == user:
-        location = chosenword_list.index(n)
-        blank_word[location] = user
-        print(n)
-    else:
-        print("Wrong")
+        if n == user:
+            location = chosenword_list.index(n)
+            blank_word[location] = user
+            print("")
+        else:
+            wrong += 1    
+            print("Wrong")
+    #if statement to keep track of lives 
+    if wrong == len(chosen_word):
+        lives -=1 
+        print(f"You can {lives} lives left")
 
-print(blank_word)
+        
+    print(blank_word) # stays
+
+print("Game Over")
+
